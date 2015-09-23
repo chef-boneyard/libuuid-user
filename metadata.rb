e9fb365e8b6e43e2d67fb8e1e91fa881c0ee073c
Tim Smith <tsmith84@gmail.com>
2015-09-23 11:57:10 -0700

    Add additional metadata

--- metadata.rb
+++ metadata.rb
@@ -3,4 +3,12 @@ maintainer       'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license          'Apache 2.0'
 description      'Set a non-login shell for the libuuid user on Ubuntu/Debian and validate that it is correct.'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.1'
+
+%w(ubuntu debian).each do |os|
+  supports os
+end
+
+source_url 'https://github.com/chef-cookbooks/libuuid-user' if respond_to?(:source_url)
+issues_url 'https://github.com/chef-cookbooks/libuuid-user/issues' if respond_to?(:issues_url)
