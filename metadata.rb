1fb6bb0977b9f6ddb67fa6a40d2e1cf4aede3248
Tim Smith <tsmith@chef.io>
2017-04-24 23:33:17 -0700

    Use SPDX standard license string
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'libuuid-user'
 maintainer       'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
-license          'Apache 2.0'
+license          'Apache-2.0'
 description      'Set a non-login shell for the libuuid user on Ubuntu/Debian and validate that it is correct.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.1'
